787d1919083f46c174b45adc3cc37872b7d22ca2
WalterSchreppers <wschrep@wschrep-Latitude-E6520.(none)>
2012-10-22 11:59:45 +0200

    Added ifdef for linux/mac code execvP and execvp are bit different...

--- src/wash.cpp
+++ src/wash.cpp
@@ -144,10 +144,13 @@ void execute_shell( const string& command ){
   }
   argv[words.size()]=(char*)NULL;
 
+#ifdef __APPLE__
   //somehow argv is not yet build up correctly TODO HERE!!!. Also the second param is the path here, we need something like PATH=$PATH:... equivalent for wash!
   const char* path = "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/CrossPack-AVR/bin";
-
   execvP( argv[0], path, argv );
+#else
+  execvp( argv[0], argv );
+#endif
 }
 
 
